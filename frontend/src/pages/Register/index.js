ed45ddaebca05c66d5419f0d637540014e1464f4
Gustavo Ueti <gustavo.ueti@gmail.com>
2021-10-22 21:36:39 -0300

    new colors on register page

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -43,11 +43,11 @@ export default function Register(){
             <div className="content">
                 <section>
                   <img src={logoImg} alt="Be The Hero"/>  
-                  <h1>Cadastro</h1>
-                  <p> Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
+                  <h1 class="titleRegister">Cadastro</h1>
+                  <p class="pRegister"> Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
                   
                   <Link className="back-link" to="/register">
-                        <FiArrowLeft size={16} color="#E02041" />
+                        <FiArrowLeft size={16} color="#7B00D6" />
                         Não tenho cadastro
                   </Link>
 
